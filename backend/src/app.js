6ffcdb3c3605112d8c5921197909d1b0e7550d92
rahulKumar1510 <er.rahulkr95@gmail.com>
2023-07-06 04:51:49 +0530

    adding security http headers

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,7 @@ configurePassport(passport);
 
 
 // set security HTTP headers
-// app.use(helmet());
+app.use(helmet());
 
 // // parse json request body
 // app.use(express.json({limit: '50mb'}));
@@ -59,29 +59,7 @@ const corsConfig = {
 app.use(cors(corsConfig));
 app.options("*", cors());
 
-// security
 
-// set security HTTP headers
-// app.use(helmet());
-
-// prevent sql injection
-app.use(mongoSanitize());
-
-
-
-// prevent cross-site scripting xss
-app.use(xssClean());
-
-// security to limit request to api endpoint per 15 min per IP address
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
-app.use(limiter);
-//HTTP Param Pollution
-app.use(hpp());
 
 //auth routes for register and login
 app.use("/auth",authRoutes);
