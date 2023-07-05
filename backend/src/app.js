2de723b9c2eac3d032c8e515d2cc5ce4e48b03e9
rahulKumar1510 <er.rahulkr95@gmail.com>
2023-07-06 05:08:21 +0530

    increased the rate limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -70,7 +70,7 @@ app.use(xssClean());
 // security to limit request to api endpoint per 15 min per IP address
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 700, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
